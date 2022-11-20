415a1092ce59b9e216d38363feebecd7ea304c3a
MuhammadGagah <muha.aku@gmail.com>
2022-11-21 05:21:06 +0700

    first commit

---
+++ src/tampilkannama/TampilkanNama.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tampilkannama;
+
+/**
+ *
+ * @author muham
+ */
+public class TampilkanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // code untuk menyimpan nama
+        String nama = "Muhammad";
+        // code untuk mencetak nama
+        System.out.println(nama);
+        // code untuk mencetak nama, ditambah dengan teks
+        System.out.println("Hai, nama anda " +nama);
+    }
+    
+}
